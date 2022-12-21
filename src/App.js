faa2c27c95600540358be6f41f83dcb14f0252eb
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-21 21:16:43 +0100

    Add Links

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Routes } from "react-router-dom";
 
 import { About } from "./components/About";
 import { Home } from "./components/Home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} com />
         <Route path="/about" element={<About />} com />
