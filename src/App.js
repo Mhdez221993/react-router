e3e05b00f9389ee3f2244bbab50d2c6a0b96fdb5
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-22 21:19:42 +0100

    Not match route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import { About } from "./components/About";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { NotMatch } from "./components/NotMatch";
 import { OrderSummary } from "./components/OrderSummary";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} com />
         <Route path="/about" element={<About />} com />
         <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NotMatch />} />
       </Routes>
     </div>
   );
---
+++ src/components/NotMatch.js
@@ -0,0 +1,7 @@
+export const NotMatch = () => {
+  return (
+    <>
+      <div>Page not found!</div>
+    </>
+  );
+};
