601c580d6a158dec9b5cf6ccabab2fd3c7dd4c7f
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-22 23:30:08 +0100

    Url params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export const UserDetails = () => {
+  const { userId } = useParams();
+
   return (
     <>
-      <div>User Details page</div>
+      <div>User Details {userId}</div>
     </>
   );
 };
