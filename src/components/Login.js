dfc9886b43139376c48c668eef2cc3859c872d74
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-23 20:15:17 +0100

    Submit user name on click

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,27 @@
+import { useAuth } from "../auth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 export const Login = () => {
+  const [user, setUser] = useState(null);
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (user != null && user !== "") {
+      login(user);
+      navigate("/");
+    }
+  };
+
   return (
     <>
-      <input type="text" />
-      <button>Submit</button>
+      <input
+        type="text"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <button onClick={handleClick}>Submit</button>
     </>
   );
 };
